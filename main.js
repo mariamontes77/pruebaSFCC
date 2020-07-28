b0f0dd472dbac1ffe8012bb2ef91af375ecb9c1f
mariamontes77 <maria.moncat@gmail.com>
2020-07-28 10:37:36 +0200

    Displaying data
    Se recorre la variable con los datos del JSON para imprimirlos en una tabla.

--- main.js
+++ main.js
@@ -2,5 +2,15 @@ $.getJSON('https://raw.githubusercontent.com/ITBconsult/json-data/master/data.js
 
    // Data contiene los datos del JSON
 
+   $(function () {
+       $.each(data, function (i, item) {
+            $('<tr>').append(
+            $('<td>').text(item.name),
+            $('<td>').text(item.city),
+            $('<td>').text(item.color),           
+            $('<td>').text(item.age)).appendTo('#table01');
+       });
+   });
+
 });
 
